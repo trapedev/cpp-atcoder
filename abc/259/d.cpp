278243a846c898976313b0b61ce2a2373f9bba57
trapedev <mvemtae5757@outlook.jp>
2022-07-15 00:34:56 +0900

    d

--- abc/259/d.cpp
+++ abc/259/d.cpp
@@ -24,7 +24,55 @@ void solve(){
 
 }
 
+int n;
+int S, T;
+ll sx, sy, tx, ty;
+ll x[101010], y[101010], r[101010];
+vector<int> G[101010];
+bool used[101010];
+
+bool dfs(int v) {
+    used[v] = true;
+    if(v == T) {
+        return true;
+    }
+    for(auto u : G[v]) {
+        if(used[u]) {
+            continue;
+        }
+        if(dfs(u)) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
-    
+    cin >> n;
+    cin >> sx >> sy >> tx >> ty;
+
+    for(int i = 1; i <= n; i++) {
+        cin >> x[i] >> y[i] >> r[i];
+    }
+
+    for(int i = 1; i <= n; i++){
+        for(int j = i+1; j <= n; j++){
+            ll d = (x[i]-x[j])*(x[i]-x[j]) + (y[i]-y[j])*(y[i]-y[j]);
+            if(d > (r[i]+r[j])*(r[i]+r[j]) || d < (r[i]-r[j])*(r[i]-r[j])) continue;
+            G[i].push_back(j), G[j].push_back(i);
+        }
+    }
+
+    for(int i = 1; i <= n; i++){
+        if((x[i]-sx)*(x[i]-sx) + (y[i]-sy)*(y[i]-sy) == r[i]*r[i]) S = i;
+        if((x[i]-tx)*(x[i]-tx) + (y[i]-ty)*(y[i]-ty) == r[i]*r[i]) T = i;
+    }
+
+    if(dfs(S)) {
+        cout << "Yes" << endl;
+    }
+    else {
+        cout << "No" << endl;
+    }
     return 0;
 }
